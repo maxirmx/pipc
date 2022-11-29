1c6f3eb7190d10c508f0158c75982228c93dc382
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2022-11-29 20:45:46 +0300

    Timing test (2)

--- test-l.php
+++ test-l.php
@@ -1,13 +1,16 @@
 <?php
+    $n = 100;
     print("Creating pipc publisher ...");
     $publisher = new PipcPublisher();
     print(" OK" . PHP_EOL);
 
+    print("Publishing " . $n . " messages ...");
     for ($i = 1; $i <= 1000; $i++) {
         $msg = "Message #" . $i;
 //        print("Sending '" . $msg . "'" . PHP_EOL );
         $publisher->send_message($msg);
         time_nanosleep(0, 50000000);
     }
-    time_nanosleep(0, 5000000);
+    print(" OK" . PHP_EOL);
+    time_nanosleep(1, 50000000);
 ?>
--- test-s.php
+++ test-s.php
@@ -5,19 +5,25 @@
 
     $on_message = function($msg) {
 //        print("Message received: '" . $msg . "'" . PHP_EOL);
+        time_nanosleep(0, 5000000);
     };
 
     print("Subscribing for messages ... ");
     $subscriber->subscribe($on_message);
     print(" OK" . PHP_EOL);
 
-    sleep(60);
+    sleep(20);
     print("Unsubscribing from messages ... ");
     $subscriber->unsubscribe();
     print(" OK" . PHP_EOL);
+
+    time_nanosleep(1, 50000000);
 
     print("Number of messages received: " . $subscriber->number_of_messages . PHP_EOL);
     print("Average delivery time: " . $subscriber->average_delivery_time . " microseconds" . PHP_EOL);
     print("Minimal delivery time: " . $subscriber->minimal_delivery_time . " microseconds" . PHP_EOL);
     print("Maximal delivery time: " . $subscriber->maximal_delivery_time . " microseconds" . PHP_EOL);
+
+    time_nanosleep(1, 50000000);
+
 ?>
